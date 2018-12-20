a696a6a99470fc9ee1b53363b79ed0d9a06a8ecd
Kerits and Letus <kerits.n.letus@gmail.com>
2018-12-20 13:44:01 -0500

    Updates

---
+++ precache-manifest.7b20593554424f4d2e38a4d2c17d3c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1047d8c345921ec0cee",
+    "url": "/static/js/main.d1047d8c.chunk.js"
+  },
+  {
+    "revision": "a47a616a6710ba7f3be8",
+    "url": "/static/js/1.a47a616a.chunk.js"
+  },
+  {
+    "revision": "d1047d8c345921ec0cee",
+    "url": "/static/css/main.5fa0c9bc.chunk.css"
+  },
+  {
+    "revision": "1f9c5c412c1e495ef86411771cad267e",
+    "url": "/index.html"
+  }
+];
