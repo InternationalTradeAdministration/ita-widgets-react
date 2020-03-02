75f21ec272c39c84e6f372f0090c9e08f34c3570
Donna Hogan <ddhogan@users.noreply.github.com>
2020-03-02 13:28:39 -0500

    [MDS-174] Dockerize and host on Azure Apps
    - Moved a couple dependencies to devDependencies to try to reduce container size

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo az acr login --name dataservices
+sudo docker login dataservices.azurecr.io -u dataservices -p $AZURE_CONTAINER_KEY  #use container username and access key
+sudo docker build -t dataservices.azurecr.io/ita-search-widgets .
+sudo docker push dataservices.azurecr.io/ita-search-widgets
